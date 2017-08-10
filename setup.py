01b5176693347ff667b9c35e3eaa271afe2a6695
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-10 21:35:34 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+import singleton_decorator
+
+setup(
+    name = "singleton_decorator",
+    version = singleton_decorator.__version__,
+    fullname = "Singleton Decorator",
+    description = "A testable singleton decorator",
+    author = "Taras Gaidukov",
+    author_email = "kemaweyan@gmail.com",
+    keywords = "singleton decorator unittest",
+    long_description = open('README.rst').read(),
+    url = "https://github.com/Kemaweyan/singleton_decorator",
+    license = "GPLv3",
+    packages=find_packages(exclude=["*.tests"])
+)
