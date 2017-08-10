f8d3a1349417e37e1dee138d1470a0b31cbadfe6
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-10 21:30:52 +0300

    Added singleton implementation

---
+++ singleton_decorator/__init__.py
@@ -0,0 +1,3 @@
+from .decorator import singleton
+
+__version__ = "1.0.0"
---
+++ singleton_decorator/decorator.py
@@ -0,0 +1,23 @@
+class _SingletonWrapper:
+    """
+    A singleton wrapper class. Its instances would be created
+    for each decorated class. 
+    """
+
+    def __init__(self, cls):
+        self.__wrapped__ = cls
+        self._instance = None
+
+    def __call__(self, *args, **kwargs):
+        """Returns a single instance of decorated class"""
+        if self._instance is None:
+            self._instance = self.__wrapped__(*args, **kwargs)
+        return self._instance
+
+def singleton(cls):
+    """
+    A singleton decorator. Returns a wrapper objects. A call on that object
+    returns a single instance object of decorated class. Use the __wrapped__
+    attribute to access decorated class directly in unit tests
+    """
+    return _SingletonWrapper(cls)
