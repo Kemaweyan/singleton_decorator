109e8a82e64c6ae76da3b884d85e8eac43374362
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-10 20:13:53 +0300

    Added test for the __call__ method

--- singleton_decorator/tests.py
+++ singleton_decorator/tests.py
@@ -23,3 +23,37 @@ class TestSingletonWrapper(TestCase):
         decorator._SingletonWrapper.__init__(self.wrapper, cls)
         # check whether the object holds wrapped class
         self.assertEqual(self.wrapper.__wrapped__, cls)
+
+    def test_call_has_not_instance(self):
+        """
+        Checks whether the __call__ method creates a new instance
+        of wrapped class and returns it if an instance has not been
+        created before
+        """
+        # set init value of _instance to None
+        self.wrapper._instance = None
+        # create a wrapped mock object
+        self.wrapper.__wrapped__ = mock.MagicMock()
+        # call the method
+        result = decorator._SingletonWrapper.__call__(self.wrapper)
+        # check whether the method returns a result of a call
+        # of the wrapped class
+        self.assertEqual(result, self.wrapper.__wrapped__.return_value)
+        # check whether the wrapped class has been called once
+        self.wrapper.__wrapped__.assert_called_once_with()
+        # check whether the created object has been held
+        self.assertEqual(result, self.wrapper._instance)
+
+    def test_call_has_instance(self):
+        """
+        Checks whether the __call__ method returns the instance created
+        before if it already exists and does not create a new one
+        """
+        # create a wrapped mock object
+        self.wrapper.__wrapped__ = mock.MagicMock()
+        # call the method
+        result = decorator._SingletonWrapper.__call__(self.wrapper)
+        # check whether the method returns a held instance
+        self.assertEqual(result, self.wrapper._instance)
+        # check whether the wrapped class has not been called
+        self.assertEqual(self.wrapper.__wrapped__.call_count, 0)
